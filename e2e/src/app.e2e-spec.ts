5d1afd5c732313ab861b2f205f32bb55dfcffcf6
DirectX <DirectX@users.noreply.github.com>
2020-12-11 23:31:44 +0400

    Angular 11

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('audio-dataset-maker app is running!');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    expect(await page.getTitleText()).toEqual('audio-dataset-maker app is running!');
   });
 
   afterEach(async () => {
